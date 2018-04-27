ff0844bb1590d76e118a9dfd8510527b7e8537ef
Valerie Polad <valeriepolad@gmail.com>
2018-04-26 22:49:12 -0700

    Initial Commit

---
+++ vector_class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vector_class
+//
+//  Created by Valerie Polad on 4/26/18.
+//  Copyright © 2018 Valerie Polad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

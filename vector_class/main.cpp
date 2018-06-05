9d76ac276de3d921f2b15593109eab59a852bee7
Valerie Polad <38049887+valeriepolad@users.noreply.github.com>
2018-06-05 16:14:45 -0700

    clean main function, disregard extra stuff for now

--- vector_class/main.cpp
+++ vector_class/main.cpp
@@ -3,11 +3,6 @@
 using namespace std;
 
 int main () {
-    int i=5, j=6, k;
-    long l=10, m=5, n;
-    k = pic10b::get_max(i,j);
-    n = pic10b::get_max(l,m);
-    cout << k << std::endl;
-    cout << n << std::endl;
+   
     return 0;
 }

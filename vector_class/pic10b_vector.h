26de61f4a1d8ba1c371c03a99c8531139a821a42
Valerie Polad <38049887+valeriepolad@users.noreply.github.com>
2018-06-05 16:13:30 -0700

    add big 4 to vector template

--- vector_class/pic10b_vector.h
+++ vector_class/pic10b_vector.h
@@ -2,14 +2,31 @@
 #define pic10b_vector_h
 
 #include <iostream>
+#include <string>
 
-namespace pic10b{
-    template <class my_type>
-    my_type get_max (my_type a, my_type b) {
-        my_type result;
-        result = (a>b)? a : b;
-        return (result);
-    }
+namespace pic10b {
+    template <class T>
+    class vector{
+    private:
+        T* the_data;
+        size_t the_size;
+        size_t the_capacity;
+        static const int INIT_CAP = 10;
+        void reserve( size_t new_capacity );
+    public:
+        bool empty() const;
+        size_t size() const;
+        size_t capacity() const;
+        double front() const;
+        double back() const;
+        double at( size_t index ) const;
+        double& operator[]( size_t index );
+        double operator[]( size_t index ) const;
+        void dump_data_to( std::ostream& out ) const;
+        void dump_data() const;
+        void push_back( double new_value );
+        void pop_back();
+    };
 }
 
 
